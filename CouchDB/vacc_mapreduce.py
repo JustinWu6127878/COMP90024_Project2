4cf3afcb7d628c1fc71d91a63b017990cb114dc4
kaichengw1 <80944419+kaichengw1@users.noreply.github.com>
2022-04-30 15:59:56 +0800

    CouchDB mapreduce

---
+++ CouchDB/vacc_mapreduce.py
@@ -0,0 +1,26 @@
+import couchdb
+
+_doc = {
+    "_id": "_design/sentiment",
+    "views": {
+        "sentiment": {
+            "map": "function(doc) { emit(doc.sentiment); }",
+            "reduce": "_count"
+        }
+    }
+}
+
+db_address = 'http://admin:123456@172.26.132.252:5984/'
+
+try:
+    couchServer =  couchdb.Server(db_address)
+except Exception as e:
+    print(e)
+
+
+db_list = ['vacc_melb', 'vacc_syd', 'vacc_bris', 'vacc_adel']
+
+for db_key in db_list:
+    db = couchServer[db_key]
+    db.save(_doc)
+    

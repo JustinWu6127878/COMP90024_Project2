bcbc100eb00b86cd8310c8ad22761b9bdb4ae5e9
JustinWu6127878 <junjie1@student.unimelb.edu.au>
2022-05-06 12:57:19 +1000

    add piechart data

---
+++ Template/.history/src/class/Covid_20220505234125.js
@@ -0,0 +1,15 @@
+class Covid {
+    constructor (hos, active, clinincs, rate, pos, neg, neu){
+        this.hos = hos;
+        this.active = active;
+        this.clinincs = clinincs;
+        this.rate = rate;
+        this.pos = pos;
+        this.neg = neg;
+        this.neu = neu;
+    }
+}
+
+
+
+export default Covid
